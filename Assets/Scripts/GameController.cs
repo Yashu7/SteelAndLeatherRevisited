64b61e350bad19a3aca88af8e15014583a4bd0bf
Jan <janmalawski2@gmail.com>
2020-12-03 21:47:11 +0100

    Adjust UiController order of execution

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
    #region fields and properties
-   public int gold;
-   public int leather;
-   public int steel;
-   public int fame;
+   public int gold = 50;
+   public int leather = 30;
+   public int steel = 30;
+   public int fame = 0;
    private Vector3 leftSlot = new Vector3(-1.5F,2,0);
    private Vector3 rightSlot =  new Vector3(1.5F,2,0);
    
@@ -25,9 +25,11 @@ public class GameController : MonoBehaviour
        EventsBroker.ClickedForgeButton += Forge;
        EventsBroker.ClickedBuyLeatherButton += BuyLeather;
        EventsBroker.ClickedBuySteelButton += BuySteel;
-      
-     leftArmor = InstatiateNewArmor(armors[0],leftSlot);
-     rightArmor = InstatiateNewArmor(armors[1],rightSlot);
+       
+       leftArmor = InstatiateNewArmor(armors[0],leftSlot);
+       rightArmor = InstatiateNewArmor(armors[1],rightSlot);
+
+       CalculatePoints();
 
    }
    #endregion
@@ -45,27 +47,52 @@ public class GameController : MonoBehaviour
    }
    private void BuyLeather()
    {
-       gold -= 10;
-       leather += 5;
+       if((gold-5)< 0) return;
+       gold -= 5;
+       leather += 1;
        CalculatePoints();
    } 
    private void BuySteel()
    {
-       gold -= 10;
-       steel += 5;
+        if((gold-5)< 0) return;
+       gold -= 5;
+       steel += 1;
        CalculatePoints();
    } 
    private void Forge()
    {
        if(clickedObject == null) return;
        var partCost = clickedObject.GetComponent<IPart>();
+       if(ValidatePayment(partCost))
+       {
        steel -= partCost.SteelCost;
        leather -= partCost.LeatherCost;
+       }
+       else{
+           return;
+       }
        
        Destroy(clickedObject);
-       
-       
-       if(leftArmor.childCount <= 1)
+       CheckIfWholeArmorRepaired();
+       CalculatePoints();
+   } 
+   private void CalculatePoints()
+   {
+       EventsBroker.CallUpdateLeather(leather);
+       EventsBroker.CallUpdateFame(fame);
+       EventsBroker.CallUpdateGold(gold);
+       EventsBroker.CallUpdateSteel(steel);
+   }
+   private bool ValidatePayment(IPart part)
+   {
+        if((steel - part.SteelCost) < 0) return false;
+        if((leather - part.LeatherCost) < 0) return false;
+        return true;
+   } 
+
+   private void CheckIfWholeArmorRepaired()
+   {
+    if(leftArmor.childCount <= 1)
        {
            gold += 50;
            fame += 10;
@@ -82,15 +109,6 @@ public class GameController : MonoBehaviour
            rightArmor = InstatiateNewArmor(armors[1],rightSlot);
             
        }
-       CalculatePoints();
-       
-   } 
-   private void CalculatePoints()
-   {
-       EventsBroker.CallUpdateLeather(leather);
-       EventsBroker.CallUpdateFame(fame);
-       EventsBroker.CallUpdateGold(gold);
-       EventsBroker.CallUpdateSteel(steel);
    }
    #endregion
 }
